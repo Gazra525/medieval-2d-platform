f26f2d6be379d36c17b52e97f4fcc7d2a047bf8d
Gazra525 <127860449+Gazra525@users.noreply.github.com>
2023-03-14 15:54:59 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.gameOver(false)
-    game.setGameOverEffect(false, effects.melt)
+	
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    game.gameOver(false)
-    game.setGameOverEffect(false, effects.melt)
+    statusbar.value += -25
+    mySprite.vy = -100
+    mySprite.vx = -25
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -80
+        mySprite.vy = -100
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile8`, function (sprite, location) {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.confetti)
 })
+let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(10)
 tiles.setCurrentTilemap(tilemap`level1`)
@@ -40,4 +41,8 @@ mySprite.ay = 200
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
 controller.moveSprite(mySprite, 100, 0)
+let Health = 100
 info.setScore(0)
+statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.attachToSprite(mySprite)
+statusbar.setColor(15, 10)
