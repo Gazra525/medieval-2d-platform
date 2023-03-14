51f8adff78aa347d6567b181654a44b36587c2b1
Gazra525 <127860449+Gazra525@users.noreply.github.com>
2023-03-14 14:23:58 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -80
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile8`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(10)
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 200
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
+controller.moveSprite(mySprite, 100, 0)
+info.setScore(0)
