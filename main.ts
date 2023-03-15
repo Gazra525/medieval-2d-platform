b3d57da74525037cee7c1db889d6651a73e45f67
Gazra525 <127860449+Gazra525@users.noreply.github.com>
2023-03-15 15:46:36 +0000

    Restore '15/03/2023, 09:14:38 statur bar colour change'

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-namespace SpriteKind {
-    export const Coin = SpriteKind.create()
-}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
 	
 })
@@ -14,22 +11,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -100
     }
 })
-function generateCoins () {
-    CoinNum = 10
-    CoinList = []
-    for (let index = 0; index < CoinNum; index++) {
-        CoinList.push(sprites.create(assets.image`Coin`, SpriteKind.Player))
-    }
-    for (let Coin of sprites.allOfKind(SpriteKind.Coin)) {
-        tiles.placeOnTile(Coin, tiles.getTileLocation(1, 2))
-    }
-}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile8`, function (sprite, location) {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.confetti)
 })
-let CoinList: Sprite[] = []
-let CoinNum = 0
 let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(10)
@@ -52,8 +37,7 @@ mySprite = sprites.create(img`
     . . . 2 2 2 2 2 2 2 2 2 2 . . . 
     . . . 2 2 2 2 2 2 2 2 2 2 . . . 
     `, SpriteKind.Player)
-let Coin = sprites.create(assets.image`Coin`, SpriteKind.Coin)
-mySprite.ay = 350
+mySprite.ay = 200
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
 controller.moveSprite(mySprite, 100, 0)
